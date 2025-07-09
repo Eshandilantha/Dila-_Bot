ab191893cf4633f7c996e5150e9b90833f969a21
Eshandilantha <eshansenadeera19@gmail.com>
2025-07-09 09:16:07 +0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ApBDDIgZ#3ksSe6m-O8dTFEY6GJSnIPvPk_NYCcJh15plHlGiucw",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Dila-Bot Is Alive Now😍*",
 BOT_OWNER: '94784398732',  // Replace with the owner's phone number
